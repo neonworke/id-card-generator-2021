4be029f839d155ecd314e61ebe5db0aea4828dc3
neonworke <amanagrawal181@gmail.com>
2021-08-04 02:46:22 +0530

    Commited changes

--- js/index.js
+++ js/index.js
@@ -15,3 +15,8 @@ function generateIdCard(event) {
     const generatedIdCardContainer = document.getElementById("generated-id-card");
     generatedIdCardContainer.style.display = "block";
 }
+
+function previewImage(event){
+    const output = document.getElementById("student-image");
+    output.src = URL.createObjectURL(event.target.files[0]);
+}
