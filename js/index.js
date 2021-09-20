73982d589564ae760537da2e1cf78f9be6df3786
neonworke <amanagrawal181@gmail.com>
2021-09-20 16:59:10 +0530

    Added icons

--- js/index.js
+++ js/index.js
@@ -17,6 +17,12 @@ function generateIdCard(event) {
 }
 
 function previewImage(event){
+    if(event.target.files.length > 0){
+        const src = URL.createObjectURL(event.target.files[0]);
+        const preview = document.getElementById("file-ip-1-preview");
+        preview.src = src;
+        preview.style.display = "block";
+    }
     const output = document.getElementById("student-image");
     output.src = URL.createObjectURL(event.target.files[0]);
 }
